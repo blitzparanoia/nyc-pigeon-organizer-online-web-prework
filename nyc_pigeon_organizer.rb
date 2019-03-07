bedfb4df11d95ddceb76b410d15cbaa9d93f9b26
Irene Saez <bluemilk44@gmail.com>
2019-03-07 22:13:35 +0000

    Automatically backed up by Learn

--- nyc_pigeon_organizer.rb
+++ nyc_pigeon_organizer.rb
@@ -2,6 +2,7 @@ require 'pry'
 def nyc_pigeon_organizer(data)
   # write your code here!
   data.each_with_object({}) do |(key, h), result|
-    binding.pry
+    h.each do |key, value|
+    #binding.pry
   end
 end
