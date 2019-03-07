4913a40e9a49d7d0d6420bbb6854f3bd1df86154
Irene Saez <bluemilk44@gmail.com>
2019-03-07 22:20:42 +0000

    Automatically backed up by Learn

--- nyc_pigeon_organizer.rb
+++ nyc_pigeon_organizer.rb
@@ -4,6 +4,7 @@ def nyc_pigeon_organizer(data)
   data.each_with_object({}) do |(key, h), result|
     h.each do |value, name|
     name.each do |name|
+      result[name] || = []
     binding.pry
     end
   end
